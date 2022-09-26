4cdadce4ee533b1f3a73b323d98123a5ddf26394
JSSatorres <juansataz.dev@gmail.com>
2022-09-26 18:49:29 +0200

    header and footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+        <span>  with <a href="//vuejs.org">Vue.js</a></span>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'app-footer'
+}
+</script>
+
+<style lang="css" scoped>
+    footer{
+        box-sizing: border-box;
+        background-color: rgb(61, 38, 83);
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        padding: 1rem;
+        width: 100%;
+        color: rgb(160, 204, 204);
+    }
+    a{
+        text-decoration: none;
+        color: aliceblue;
+    }
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <span class="title" > Vue Trello</span>
+        <router-link class="btn-header" to="/"></router-link>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'app-header'
+}
+</script>
+
+<style lang="css" scoped>
+    header{
+        box-sizing: border-box;
+        background-color: rgb(61, 38, 83);
+        color :white;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        margin: 0;
+        padding: 1rem;
+    }
+    .title{
+        font-size: 20px;
+        text-align: center;
+    }
+</style>
