3a955baacb1744782fe8cdf79af271421e285aae
JSSatorres <juansataz.dev@gmail.com>
2022-10-12 09:08:28 +0200

    inital state vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,14 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 export default new Vuex.Store({
-  state: {},
+  state: {
+    count: 0
+  },
   getters: {},
   actions: {},
-  mutations: {}
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  }
 })
